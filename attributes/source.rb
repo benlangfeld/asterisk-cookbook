089eff3a5af26b739005388be5472d2bc0f540bb
Bob Ziuchkovski <bob.ziuchkovski@gmail.com>
2013-08-15 22:37:10 -0600

    Move source attributes to separate file

---
+++ attributes/source.rb
@@ -0,0 +1,4 @@
+default['asterisk']['source']['packages'] = %w{libssl-dev libncurses5-dev libnewt-dev libxml2-dev libsqlite3-dev}
+default['asterisk']['source']['version']  = '11.3.0'
+default['asterisk']['source']['checksum'] = '35b7dd5d21449384ebcab631915a844d631676ff6535a6c14bd2bac819808f09'
+default['asterisk']['source']['url']      = nil
